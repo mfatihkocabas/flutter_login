be639ee2e7cf348595511eda5ae9434795f101b0
mfatihkocabas <kocabasmu16@itu.edu.tr>
2020-09-18 13:54:32 +0300

    fifth commit

--- lib/login.dart
+++ lib/login.dart
@@ -27,85 +27,100 @@ class Login extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black12,
         title: Text('Login screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Form(
-                  key: _fKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Email:",
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      TextFormField(
-                          validator:( value) {
-                        if(value.isEmpty){
-                          return 'Email is required';
-                        }
-                        if(!RegExp("^[a-zA-Z0-9.!#%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*").hasMatch(value)){
-                          return 'Enter a valid email address';
-                        }
-                        return null;
+      body: Container(
+        decoration: BoxDecoration(
+            image:DecorationImage(image:AssetImage('images/20.png'),
+              fit:BoxFit.fill,)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10,100,10,50),
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Form(
+                    key: _fKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Email:",
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                            validator:( value) {
+                          if(value.isEmpty){
+                            return 'Email is required';
+                          }
+                          if(!RegExp("^[a-zA-Z0-9.!#%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*").hasMatch(value)){
+                            return 'Enter a valid email address';
+                          }
+                          return null;
+                          },
+                            controller: mailController,
+                            obscureText: false,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Text(
+                          "Password :",
+                          style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter a valid password!';
+                          }
+                          return null;
                         },
-                          controller: mailController,
-                          obscureText: false,
+                            controller: passController,
+                            obscureText: true,
                           decoration: InputDecoration(
-                              border: InputBorder.none,
-                              filled: true),),
-                      Text(
-                        "Password :",
-                        style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Enter a valid password!';
-                        }
-                        return null;
-                      },
-                          controller: passController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              filled: true))
-                    ],
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text('Login'),
-                onPressed: (){
-                  _sbt();
-                  SharedPrefs.saveMail(mailController.text);
-                  SharedPrefs.savePassword(passController.text);
-                  if(emailController.text==mailController.text && passwordController.text==passController.text) {
-                    if (SharedPrefs.saveMail(mailController.text) != null && SharedPrefs.savePassword(passController.text) != null){
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Homepage()));
-                  }}
+                SizedBox(height: 20,),
+                RaisedButton(
+                  color: Colors.deepPurple,
+                  child: Text('Login'),
+                  onPressed: (){
+                    _sbt();
+                    SharedPrefs.saveMail(mailController.text);
+                    SharedPrefs.savePassword(passController.text);
+                    if(emailController.text==mailController.text && passwordController.text==passController.text) {
+                      if (SharedPrefs.saveMail(mailController.text) != null && SharedPrefs.savePassword(passController.text) != null){
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Homepage()));
+                    }}
 
-                  // Navigator.push(context, MaterialPageRoute(builder: (context){
-                  //  return Homepage();
-                  //},),);
-                },
-              ),
-            ],
+                    // Navigator.push(context, MaterialPageRoute(builder: (context){
+                    //  return Homepage();
+                    //},),);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/register.dart
+++ lib/register.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pathp1/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,9 +12,8 @@ var nameController = TextEditingController();
 var surnameController = TextEditingController();
 
 class Register extends StatelessWidget {
-
- final _fKey = GlobalKey<FormState>();
- void _sbt() {
+  final _fKey = GlobalKey<FormState>();
+  void _sbt() {
     final isValid = _fKey.currentState.validate();
     if (!isValid) {
       return _sbt();
@@ -26,131 +26,174 @@ class Register extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black12,
         title: Text('Register Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-
-                  margin: EdgeInsets.symmetric(vertical: 10),
-
-                  child: Form(
-                    key:_fKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Name:",
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/20.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Form(
+                        key: _fKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Name:",
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              onFieldSubmitted: (value) {
+                                //Validator
+                              },
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Enter a valid Name!';
+                                }
+                                return null;
+                              },
+                              controller: nameController,
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Password :",
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                              onFieldSubmitted: (value) {
+                                //Validator
+                              },
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Enter a valid password!';
+                                }
+                                return null;
+                              },
+                              controller: passwordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                                  ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Text(
+                              "Email:",
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            TextFormField(
+                              onFieldSubmitted: (value) {
+                                //Validator
+                              },
+                              validator: (String value) {
+                                if (value.isEmpty) {
+                                  return 'Email is required';
+                                }
+                                if (!RegExp(
+                                        "^[a-zA-Z0-9.!#%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*")
+                                    .hasMatch(value)) {
+                                  return 'Enter a valid email address';
+                                }
+                                return null;
+                              },
+                              controller: emailController,
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 50),
+                            Text(
+                              "Surname",
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                                onFieldSubmitted: (value) {
+                                  //Validator
+                                },
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Enter a valid Surname!';
+                                  }
+                                  return null;
+                                },
+                                controller: surnameController,
+                                obscureText: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0),)
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                            onFieldSubmitted: (value) {
-                              //Validator
-                            },
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Enter a valid Name!';
-                              }
-                              return null;
-                            },
-                            controller: nameController,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none, filled: true),),
-                        Text(
-                          "Password :",
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                            onFieldSubmitted: (value) {
-                              //Validator
-                            },
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Enter a valid password!';
-                              }
-                              return null;
-                            },
-
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: InputBorder.none, filled: true),),
-                        Text(
-                          "Email:",
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                            onFieldSubmitted: (value) {
-                              //Validator
-                            },
-                            validator:(String value) {
-                              if(value.isEmpty){
-                                return 'Email is required';
-                              }
-                              if(!RegExp("^[a-zA-Z0-9.!#%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*").hasMatch(value)){
-                                return 'Enter a valid email address';
-                              }
-                              return null;
-                            },
-                            controller: emailController,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none, filled: true),),
-                        Text(
-
-                          "Surname",
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                            onFieldSubmitted: (value) {
-                              //Validator
-                            },
-
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Enter a valid Surname!';
-                              }
-                              return null;
-                            },
-                            controller: surnameController,
-
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none, filled: true))
-
-                      ],
+                      ),
                     ),
                   ),
-                ),
-                RaisedButton(
-                  color: Colors.red,
-                  child: Text('Register'),
-                  onPressed: () { _sbt();
-                   SharedPrefs.saveMail(emailController.text);
-                   SharedPrefs.savePassword(passwordController.text);
-                  SharedPrefs.saveName(nameController.text);
-                   SharedPrefs.saveSurName(surnameController.text);
-                   Navigator.push(
-                     context, MaterialPageRoute(builder: (context) {
-                     return Login();
-                   },),);
-
-                  } ),
-              ],
+                  SizedBox(
+                    height: 60,
+                  ),
+                  RaisedButton(
+                      color: Colors.deepPurple,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text('Register'),
+                          ],
+                        ),
+                      ),
+                      onPressed: () {
+                        _sbt();
+                        SharedPrefs.saveMail(emailController.text);
+                        SharedPrefs.savePassword(passwordController.text);
+                        SharedPrefs.saveName(nameController.text);
+                        SharedPrefs.saveSurName(surnameController.text);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Login();
+                            },
+                          ),
+                        );
+                      }),
+                ],
+              ),
             ),
           ),
         ),
@@ -158,4 +201,3 @@ class Register extends StatelessWidget {
     );
   }
 }
-
